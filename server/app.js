1d91f063788a919fae52b812ee23ddd48525e548
danielfb124 <danielfw4k@gmail.com>
2025-06-16 17:55:52 -0500

    Conexión Azure + puerto dinámico para Render

--- server/app.js
+++ server/app.js
@@ -135,6 +135,7 @@ app.get('/animals/:id', async (req, res) => {
   
 
 
-server.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
--- server/database.js
+++ server/database.js
@@ -9,6 +9,9 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    ssl: {
+        rejectUnauthorized: true
+    }
 }).promise();
 
 
